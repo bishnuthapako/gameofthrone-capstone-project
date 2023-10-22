ea004a165ebe2ed8574c5f93d65223368832095e
bishnu.email.usa@gmail.com <bishnu.email.usa@gmail.com>
2023-10-22 07:51:08 -0400

    font added

---
+++ src/components/Pagination/Paginations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Paginations = () => {
+  return (
+    <div>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati provident officiis hic commodi possimus consequatur ea eligendi molestiae incidunt, numquam tempora, facere fugit aut ab sequi, esse laborum voluptate. Cupiditate ducimus quod sequi nihil praesentium reiciendis, velit labore error repellendus. Esse reprehenderit impedit tempora doloribus dolorum reiciendis dolorem cupiditate?</h2>
+    </div>
+  )
+}
+
+export default Paginations
--- src/pages/Characters/Characters.js
+++ src/pages/Characters/Characters.js
@@ -1,31 +1,30 @@
 import React from "react";
 import { Container, Card, Row, Col, Button } from "react-bootstrap";
 import styles from "./Characters.module.css";
+import Paginations from "../../components/Pagination/Paginations";
 
 const Characters = ({ data }) => {
   console.log(data, "char-data");
 
   return (
-    <Container className={`text-center text-white ${styles.characters}`}>
-      <Row className="mt-3">
-        {data.length !== 0 && data.map((items, index) => {
-          return (
-            <Col lg={6} md={6} sm={6} xs={12} className="mb-3">
-              <h1>{items.aliases[0]}</h1>
-              <h5>{items.gender}</h5>
-              {/* <Card className="bg-secondary">
-                <Card.Body>
-                  <Card.Title>{items.culture}</Card.Title>
-                  <Card.Text>
-                    {items.aliases[0]}
-                  </Card.Text>
-                </Card.Body>
-              </Card> */}
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
+    <>
+      <Container className={`text-center text-white ${styles.characters}`}>
+        <Row className="mt-3">
+          {data.length !== 0 &&
+            data.map((items, index) => {
+              return (
+                <Col key={index} lg={6} md={6} sm={6} xs={12} className="mb-3">
+                  <h1>{items.aliases[0]}</h1>
+                  <h5>{items.gender}</h5>
+                </Col>
+              );
+            })}
+        </Row>
+        <Row>
+          <Paginations />
+        </Row>
+      </Container>
+    </>
   );
 };
 
