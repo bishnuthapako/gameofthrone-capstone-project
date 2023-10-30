6e0ffae0f57058fb4a7e694c2ca7e61f9f60dec5
bishnu.email.usa@gmail.com <bishnu.email.usa@gmail.com>
2023-10-30 12:29:49 -0400

    footer added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "./Footer.module.css";
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      Copyright © Bishnu Bahadur Thapa @{new Date().getFullYear()}
+    <div className={`p-3 ${styles.footer}`}>
+      <p className="text-center fw-bolder">Copyright © Bishnu Bahadur Thapa @{new Date().getFullYear()} <br/>
+      <span className="fst-italic fs-6">Note: This project was created by Bishnu Thapa; credit goes to Springboard and my mentor, Renish</span>
+      </p>
     </div>
   );
 };
